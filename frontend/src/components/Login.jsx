a8f505abe8ad595874f745535aae06258e9f5c13
Ankush-Matre <ankush.matre.cs@ghrcem.raisoni.net>
2026-07-19 01:05:14 +0530

    Setup React routing and login page state

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import "../styles/login.css";
+function Login() {
+
+    const [username, setUsername] = useState("");
+
+    const handleJoin = () => {
+        console.log(username);
+    };
+    return (
+        <div className="login-container">
+
+            <div className="login-card">
+
+                <h1>💬 Live Chat</h1>
+
+                <p>Welcome Back</p>
+
+                <input
+                    type="text"
+                    placeholder="Enter Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+
+                <button onClick={handleJoin}>
+                    Join Chat
+                </button>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Login;
